324746569f35a65dfcefc86beb401e6a967d167b
leonardchiu <chiu.leonard@gmail.com>
2020-06-22 08:58:17 +0800

    second push

--- components/homepage/cards.vue
+++ components/homepage/cards.vue
@@ -9,13 +9,13 @@
         </v-card>
       </v-col>
       <v-col sm="12" md="6" lg="4">
-        <v-card height="200px" color="purple" class="pa-4">Aiko Wong</v-card>
+        <v-card dark tile elevation="10" height="200px" color="blue lighten-10" class="pa-4"><v-icon left>mdi-flower</v-icon>Aiko Wong</v-card>
       </v-col>
       <v-col sm="12" md="6" lg="4">
-        <v-card height="200px" color="purple" class="pa-4">Declan Chiu</v-card>
+        <v-card dark tile elevation="10" height="200px" color="blue lighten-10" class="pa-4">Declan Chiu</v-card>
       </v-col>
       <v-col sm="12" md="6" lg="4">
-        <v-card height="200px" color="purple" class="pa-4">Dylan Chiu</v-card>
+        <v-card dark tile elevation="10" height="200px" color="blue lighten-10" class="pa-4">Dylan Chiu</v-card>
       </v-col>
     </v-row>
   </v-container>
