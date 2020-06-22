95ac035077f4c59d5e355a4a62a82a9af38af4cb
leonardchiu <chiu.leonard@gmail.com>
2020-06-22 11:00:40 +0800

    Add viewpoint hidden for nav items but not working

--- components/layoutComp/header.vue
+++ components/layoutComp/header.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-app-bar flat fixed>
-    <v-toolbar-title>Leonard Chiu</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items>
-      <v-btn to="/" text>Home</v-btn>
-      <v-btn to="/blog" text>Blog</v-btn>
-      <v-btn to="/contact" text>Contact Us</v-btn>
-    </v-toolbar-items>
-  </v-app-bar>
+  <div>
+    <v-app-bar>
+      <v-toolbar-title><h3>LEONARD CHIU</h3></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <span class="hidden-sm-and-up">
+        <v-btn>
+          Menu
+        </v-btn>
+      </span>
+      <v-toolbar-items class="hidden-sm-and-up">
+        <v-btn to="/" text>Home</v-btn>
+        <v-btn to="/blog" text>Blog</v-btn>
+        <v-btn to="/contact" text>Contact Us</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </div>
 </template>
