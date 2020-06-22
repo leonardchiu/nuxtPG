b596900136308b732016b2ea64044a301c09ab32
leonardchiu <chiu.leonard@gmail.com>
2020-06-22 15:31:31 +0800

    Add navigation panel

--- components/layoutComp/header.vue
+++ components/layoutComp/header.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <v-app-bar>
-      <v-toolbar-title><h3>LEONARD CHIU</h3></v-toolbar-title>
+  <v-app>
+    <v-app-bar fixed flated>
+      <v-toolbar-title>
+        <h3>LEONARD CHIU</h3>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <span class="hidden-sm-and-up">
-        <v-btn>
-          Menu
-        </v-btn>
+        <v-btn>Menu</v-btn>
       </span>
       <v-toolbar-items class="hidden-sm-and-up">
         <v-btn to="/" text>Home</v-btn>
@@ -14,5 +14,8 @@
         <v-btn to="/contact" text>Contact Us</v-btn>
       </v-toolbar-items>
     </v-app-bar>
-  </div>
+    <v-card height="300px" width="600px" color="red">
+      <v-card-title class="mt-30">Hello World</v-card-title>
+    </v-card>
+  </v-app>
 </template>
--- layouts/secondary.vue
+++ layouts/secondary.vue
@@ -1,16 +1,36 @@
 <template>
-  <div>
-    <Header />
+  <v-app>
+    <v-app-bar fixed flat>
+      <v-navigation-drawer height="500px" absolute right class="hidden-sm-and-up">
+        <v-list class="ma-5">
+          <v-list-item>
+            <v-icon left>home</v-icon>Home
+          </v-list-item>
+          <v-list-item>
+            <v-icon left>opacity</v-icon>Blog
+          </v-list-item>
+          <v-list-item>
+            <v-icon left>edit</v-icon>Contact
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar-title>
+        <h3 class="ml-10">LEONARD CHIU</h3>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <span class="hidden-sm-and-up">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </span>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn to="/" text>Home</v-btn>
+        <v-btn to="/blog" text>Blog</v-btn>
+        <v-btn to="/contact" text>Contact Us</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
     <nuxt />
-  </div>
+  </v-app>
 </template>
 
 <script>
-import Header from "../components/layoutComp/header.vue";
-
-export default {
-  components: {
-    Header
-  }
-};
+export default {};
 </script>
