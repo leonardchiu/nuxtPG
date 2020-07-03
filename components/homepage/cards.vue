7354eb6552dec42bd701481952704b8d76022a1e
leonardchiu <chiu.leonard@gmail.com>
2020-07-03 11:03:41 +0800

    sorted out the v-for for both posts and images

--- components/homepage/cards.vue
+++ components/homepage/cards.vue
@@ -1,15 +1,20 @@
 <template>
   <v-container class="px-10">
     <v-row>
-      <v-col cols="12" md="6" lg="4" v-for="blog in blogs.data" :key="blog.Id">
+      <v-col cols="12" md="6" lg="4" v-for="(blog, i) in blogs.data" :key="blog.Id">
         <v-card dark tile elevation="10" color="blue lighten-10" class="pa-4">
           <v-card-title>
             <v-icon left>face</v-icon>
             {{blog.Title}}
           </v-card-title>
           <v-card-text>{{ blog.Post }}</v-card-text>
-          <v-card-text>{{ blog.Images.url }}</v-card-text>
-          <v-img :src="'http://localhost:1337' + blog.Images.url" max-width="200px"></v-img>
+          <!-- <v-card-text v-for="image in blog.Images" :key="image">{{ image.url }}</v-card-text> -->
+          <v-img
+            v-for="image in blog.Images"
+            :key="image.id"
+            :src="'http://localhost:1337' + image.url"
+            max-width="200px"
+          ></v-img>
         </v-card>
       </v-col>
       <!-- <v-col cols="12" md="6" lg="4">
@@ -55,7 +60,7 @@ export default {
   },
   mounted() {
     return axios.get("http://localhost:1337/blogs").then(response => {
-      const json = JSON.stringify(response);
+      // const json = JSON.stringify(response);
       this.blogs = response;
       console.log(response.data);
     });
