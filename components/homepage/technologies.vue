958e284a10f48742651325dc867082559b24ad7f
leonardchiu <chiu.leonard@gmail.com>
2020-07-19 19:03:57 +0800

    Change image files path at technologies component

--- components/homepage/technologies.vue
+++ components/homepage/technologies.vue
@@ -6,11 +6,7 @@
           <h1 class="text-decoration-underline">Low Code Platforms</h1>
         </v-row>
         <v-row justify="center">
-          <v-img
-            :src="require('@/assets/img/FileMaker-Logo.png')"
-            max-width="200px"
-            class="my-8"
-          ></v-img>
+          <v-img :src="require('@/assets/img/FileMaker-Logo.png')" max-width="200px" class="my-8"></v-img>
         </v-row>
       </v-col>
       <v-col cols="12" sm="6">
@@ -19,87 +15,87 @@
         </v-row>
         <v-col cols="12">
           <v-row>
-            <v-col cols="4"
-              ><v-row justify="center"
-                ><h2 class="mt-8">openRPA</h2></v-row
-              ></v-col
-            >
-            <v-col
-              ><v-row justify="center"
-                ><v-img
-                  :src="require('@/assets/img/AutoHotKeys.png')"
+            <v-col cols="4">
+              <v-row justify="center">
+                <h2 class="mt-8">openRPA</h2>
+              </v-row>
+            </v-col>
+            <v-col>
+              <v-row justify="center">
+                <v-img
+                  :src="require('../../assets/img/AutoHotKeys.png')"
                   max-width="80px"
                   class="my-2"
-                ></v-img></v-row
-            ></v-col>
-            <v-col
-              ><v-row justify="center"
-                ><v-img
-                  :src="require('@/assets/img/UIVision.jpg')"
+                ></v-img>
+              </v-row>
+            </v-col>
+            <v-col>
+              <v-row justify="center">
+                <v-img
+                  :src="require('../../assets/img/UIVision.jpg')"
                   max-width="80px"
                   class="my-2"
-                >
-                </v-img></v-row
-            ></v-col>
+                ></v-img>
+              </v-row>
+            </v-col>
           </v-row>
         </v-col>
       </v-col>
-      </v-row>
+    </v-row>
 
-      <v-row>
-        <v-col cols="12">
-          <v-row justify="center">
-            <h1 class="text-decoration-underline">Technologies I worked with...</h1>
-          </v-row>
-        </v-col>
-      </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-row justify="center">
+          <h1 class="text-decoration-underline">Technologies I worked with...</h1>
+        </v-row>
+      </v-col>
+    </v-row>
 
+    <v-row>
+      <v-col cols="6" md="3">
+        <v-row justify="center">
+          <v-img :src="require('../../assets/img/NodeJS2.png')" max-width="120px"></v-img>
+        </v-row>
+      </v-col>
+      <v-col cols="6" md="3">
+        <v-row justify="center">
+          <v-img :src="require('../../assets/img/JS.png')" max-width="80px"></v-img>
+        </v-row>
+      </v-col>
+      <v-col cols="6" md="3">
+        <v-row justify="center">
+          <v-img :src="require('../../assets/img/VueJS.png')" max-width="80px"></v-img>
+        </v-row>
+      </v-col>
+      <v-col cols="6" md="3">
+        <v-row justify="center">
+          <v-img :src="require('../../assets/img/NuxtJS.png')" max-width="100px"></v-img>
+        </v-row>
+      </v-col>
+    </v-row>
 
-      <v-row>
-        <v-col cols="6" md="3">
-           <v-row justify="center">
-            <v-img :src="require('@/assets/img/NodeJS2.png')" max-width="120px"></v-img>
-          </v-row>
-        </v-col>
-        <v-col cols="6" md="3">
-           <v-row justify="center">
-            <v-img :src="require('@/assets/img/JS.png')" max-width="80px"></v-img>
-          </v-row>
-        </v-col>
-        <v-col cols="6" md="3">
-           <v-row justify="center">
-            <v-img :src="require('@/assets/img/VueJS.png')" max-width="80px"></v-img>
-          </v-row>
-        </v-col>
-        <v-col cols="6" md="3">
-           <v-row justify="center">
-            <v-img :src="require('@/assets/img/NuxtJS.png')" max-width="100px"></v-img>
-          </v-row>
-        </v-col>
-      </v-row>
-
-      <v-row class="mb-8">
-        <v-col cols="6" md="3">
-          <v-row justify="center">
-            <v-img :src="require('@/assets/img/mongoDB2.png')" max-width="180px"></v-img>
-          </v-row>
-        </v-col>
-        <v-col cols="6" md="3">
-          <v-row justify="center">
-            <v-img :src="require('@/assets/img/FireBase.png')" max-width="60px"></v-img>
-          </v-row>
-        </v-col>
-        <v-col cols="6" md="3">
-          <v-row justify="center">
-            <v-img :src="require('@/assets/img/html-css.png')" max-width="130px"></v-img>
-          </v-row>
-        </v-col>
-        <v-col cols="6" md="3">
-          <v-row justify="center">
-            <v-img :src="require('@/assets/img/Vuetify.png')" max-width="70px"></v-img>
-          </v-row>
-        </v-col>
-      </v-row>
+    <v-row class="mb-8">
+      <v-col cols="6" md="3">
+        <v-row justify="center">
+          <v-img :src="require('../../assets/img/mongoDB2.png')" max-width="180px"></v-img>
+        </v-row>
+      </v-col>
+      <v-col cols="6" md="3">
+        <v-row justify="center">
+          <v-img :src="require('../../assets/img/FireBase.png')" max-width="60px"></v-img>
+        </v-row>
+      </v-col>
+      <v-col cols="6" md="3">
+        <v-row justify="center">
+          <v-img :src="require('../../assets/img/html-css.png')" max-width="130px"></v-img>
+        </v-row>
+      </v-col>
+      <v-col cols="6" md="3">
+        <v-row justify="center">
+          <v-img :src="require('../../assets/img/Vuetify.png')" max-width="70px"></v-img>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
