c9ed202cbfe837c3247fdfb92a13101a4bfdcc33
leonardchiu <chiu.leonard@gmail.com>
2020-07-19 21:13:08 +0800

    Change components filenames

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -7,7 +7,7 @@
 
 <script>
 import axios from "axios";
-import BlogCard from "../../components/homepage/blogCards.vue";
+import BlogCard from "../../components/homepage/BlogCards.vue";
 
 export default {
   layout: "secondary",
