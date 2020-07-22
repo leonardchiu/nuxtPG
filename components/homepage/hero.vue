80c4b425a6477f310c27f6b4e79d43e7c7c56d31
leonardchiu <chiu.leonard@gmail.com>
2020-07-22 14:59:12 +0800

    Added favicon title

--- components/homepage/hero.vue
+++ components/homepage/hero.vue
@@ -1,22 +1,23 @@
 <template>
-  <!-- <v-sheet height="400px" color="orange"> -->
-  <!-- <v-container class="fill-height">
-      <v-row justify="center" align="center">
-        <h1>Hello World!</h1>
-      </v-row>
-    </v-container> -->
-  <v-img :src="require('@/assets/img/hero.jpg')" height="600px">
-    <v-container>
-      <v-row>
-        <v-col cols="8" sm="6" md="4" lg="3" xl="3" class="mt-16 pt-16 ml-6">
-          <v-img :src="require('@/assets/img/leonardchiu.png')"></v-img>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-img>
-  <!-- </v-sheet> -->
+  <v-container fluid fill-height class="ma-0 pa-0 full">
+    <v-img :src="require('@/assets/img/hero.jpg')" height="600px">
+      <v-container>
+        <v-row>
+          <v-col cols="8" sm="6" md="4" lg="3" xl="3" class="mt-16 pt-16 ml-6">
+            <v-img :src="require('@/assets/img/leonardchiu.png')"></v-img>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-img>
+  </v-container>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style scoped>
+#full {
+  width: 100%;
+}
+</style>
